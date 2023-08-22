3dbf9fd3d1a856f8e18676a6411d4cc1397a53ec
deepaknegi00 <deepak.negi0158@gmail.com>
2023-08-22 18:51:56 +0530

    first-commit

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,11 @@ export default function Home() {
         />
       </div>
 
+      <div>
+        <a href="/blogs">click here</a>
+        <a href="/pages/sign-in">Sign In</a>
+      </div>
+
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
---
+++ app/pages/sign-in/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+    return (
+      <div>
+          Sign In
+      </div>
+    )
+  }
+  
